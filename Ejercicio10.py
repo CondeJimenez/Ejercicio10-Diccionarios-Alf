4116c5670b43ab42f5ef63ad21b0760757522ff4
Kevin Conde <conde.jimenez@hotmail.com>
2022-07-27 17:47:46 -0500

    Cambio de rama

--- Ejercicio10.py
+++ Ejercicio10.py
@@ -20,3 +20,22 @@
 #   4.Mostrar lista de todos los clientes de la base datos con su NIF y nombre.
 #   5.Mostrar la lista de clientes preferentes de la base de datos con su NIF y nombre.
 #   6.Terminar el programa.
+
+cliente = {"NIF": {"Nombre":"Kevin Conde", "Direccion": "Cerro del potosi 1241", "Telefono": 8121523309, "Correo": "conde.jimenez@hotmail.com", "Preferente":True}}
+cliente = {}
+terminar = 0
+
+print("Elige una de las siguientes opciones.")
+print("""
+Presiona 1 para Añadir Cliente.
+Presiona 2 para Eliminar Cliente.
+Presiona 3 para Mostrar Cliente.
+Presiona 4 para Listar todos los Clientes.
+Presiona 5 para Listar Clientes Preferentes.
+Presiona 6 para Terminar. 
+""")
+print()
+
+while terminar != 6:
+    menu = int(input("Elige una opcion: "))
+    if menu == 1:
